dd943f75fdc82d97bfae5534366b5e56873a03b9
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2022-05-19 11:52:17 +0100

    adjustment of images on testimonial card

--- js/script.js
+++ js/script.js
@@ -1,8 +1,57 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
+console.log(nav)
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+
+const dots = document.querySelectorAll(".dot");
+
+console.log(dots)
+var kounter = 1;
+dots.forEach(element => {
+
+    element.addEventListener('click', (e) => {
+        console.log(e.currentTarget.dataset.id)
+        showSlides(e.currentTarget.dataset.id)
+    })
+
+    kounter++;
+});
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) {slideIndex = 1}
+    
+    if (n < 1) {slideIndex = slides.length}
+    //console.log(slideIndex)
+    //console.log(n)
+    for (i = 0; i < slides.length; i++) {
+        if(slides[i].classList.contains('hidden')){
+            continue
+        }
+        slides[i].classList.add('hidden'); 
+    }
+    for (i = 0; i < dots.length; i++) {
+        if(dots[i].classList.contains('bg-brightRed')){
+            dots[i].classList.remove('bg-brightRed')
+        }
+       ; 
+    }
+     //console.log(slides)
+    slides[n-1].classList.remove('hidden');  
+    dots[n-1].classList.add("bg-brightRed");
+  }
